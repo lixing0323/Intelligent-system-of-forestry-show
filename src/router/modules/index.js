6fa9369450011fab9035819bb9112718449d4f2b
李星 <lixing@nibatech.cn>
2022-07-07 10:34:22 +0800

    removed data router

--- src/router/modules/index.js
+++ src/router/modules/index.js
@@ -1,13 +1,11 @@
 import { cityRouter } from './city'
 import { countryRouter } from './country'
 import { plotRouter } from './plot'
-import dataRouter from './data'
 
 const asyncRoutes = [
   cityRouter,
   countryRouter,
-  plotRouter,
-  dataRouter
+  plotRouter
 ]
 
 export default asyncRoutes
